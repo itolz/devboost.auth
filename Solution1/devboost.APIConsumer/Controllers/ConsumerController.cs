f49ec69d3d77e49b2f1bf4bf97f0aaeec4926009
italovinicios <italovinicios@gmail.com>
2020-08-28 21:37:00 -0300

    autenticação SSO

---
+++ Solution1/devboost.APIConsumer/Controllers/ConsumerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devboost.APIConsumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+       
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
---
+++ Solution1/devboost.SSO/Config.cs
@@ -0,0 +1,59 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devboost.SSO
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "devboostSSO",
+                    DisplayName = "devboost SSO",
+                    Scopes = { "Business" }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+
+            return new List<Client>
+            {
+
+                //Client-Credential GrantType
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "Business" },
+                    Enabled = true,
+                    AllowOfflineAccess = true
+                }
+
+
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("Business", "devboost SSO")
+            };
+        }
+
+
+
+    }
+}
